6e75abb94abb184d6bb377865cf430a3148a867b
pmpr.dev <pmpr.dev@gmail.com>
2024-10-14 22:16:23 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d4ad601870             |
+    |             670d98130cf78             |
     |_______________________________________|
 */
  use Pmpr\Module\Security\Security; Security::symcgieuakksimmu();
--- src/Pmpr/Module/Security/Setting/SettingSegment.php
+++ src/Pmpr/Module/Security/Setting/SettingSegment.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d4ad601870             |
+    |             670d98130cf78             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Security\Setting; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Segment; abstract class SettingSegment extends Segment { public function ikcgmcycisiccyuc() { $this->setting = Setting::symcgieuakksimmu(); } }
