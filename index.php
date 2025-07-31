fbeeed0a45b36821dc6f40c4b1298624454e2b95
pmpr.dev <pmpr.dev@gmail.com>
2025-07-31 12:52:48 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             682fc6db4685c             |
+    |             688b66f17f79d             |
     |_______________________________________|
 */
  use Pmpr\Module\Security\Security; Security::symcgieuakksimmu();
--- src/Pmpr/Module/Security/Setting/SettingSection.php
+++ src/Pmpr/Module/Security/Setting/SettingSection.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             682fc6db4685c             |
+    |             688b66f17f79d             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Security\Setting; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Section; class SettingSection extends Section { public function ikcgmcycisiccyuc() { $this->setting = Setting::symcgieuakksimmu(); } }
