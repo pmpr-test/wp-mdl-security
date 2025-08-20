fc8823c9124d6d1ae92827cf4bee13b94a153e76
pmpr.dev <pmpr.dev@gmail.com>
2025-08-20 21:26:21 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             688b675a3aedb             |
+    |             68a63d4f7bb20             |
     |_______________________________________|
 */
  use Pmpr\Module\Security\Security; Security::symcgieuakksimmu();
--- src/Pmpr/Module/Security/Defender/Defender.php
+++ src/Pmpr/Module/Security/Defender/Defender.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             688b675a3aedb             |
+    |             68a63d4f7bb20             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Security\Defender; use Pmpr\Module\Security\Container; use Pmpr\Module\Security\Defender\Auth\Auth; use Pmpr\Module\Security\Defender\Config\Config; use Pmpr\Module\Security\Defender\Resource\Resource; class Defender extends Container { public function mameiwsayuyquoeq() { Auth::symcgieuakksimmu(); Config::symcgieuakksimmu(); Resource::symcgieuakksimmu(); } }
